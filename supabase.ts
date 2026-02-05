356e14f9f3ddb6a13fd224a00562c130a6d17b91
FPSU <artipl.batreg@gmail.com>
2026-02-05 07:41:39 +0700

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Konfigurasi Database GNOME COMP-TEST DRIVE
-const supabaseUrl = 'https://ovdlcxrmrxizhmpghbhx.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92ZGxjeHJtcnhpemhtcGdoYmh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMjY4ODcsImV4cCI6MjA4NTgwMjg4N30.qOGKuBBD3OUPpsBbt6DxYPDZSh4YK8Mbv3Bnh8A28KE';
+const supabaseUrl = 'https://eywjayeilwzwtkqswnfi.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5d2pheWVpbHd6d3RrcXN3bmZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5MDg1MjAsImV4cCI6MjA4NTQ4NDUyMH0.RZEU8_u5Jfrp_ZP1WVcQV8VbNjG8Yvr005N5ORXWhlE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
